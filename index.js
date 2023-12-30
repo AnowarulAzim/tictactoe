6fdef80438c01a42a0592b467642651b2172ac91
AnowarulAzim <82867005+AnowarulAzim@users.noreply.github.com>
2023-12-30 21:50:27 +0600

    trial push version 1

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 let val = ['black','white']
 let docs = ['x.png','o.png']
 let i = 0;
@@ -41,4 +42,49 @@ function update(imageID) {
 		document.getElementById("finish").innerHTML = "GAME FINISHED";
 	}
 	finish = 0;
+=======
+let val = ['black','white']
+let docs = ['x.png','o.png']
+let i = 0;
+let k = 0;
+let finish=0;
+let visited = { 'image1':0,
+				'image2':0,
+				'image3':0,
+				'image4':0,
+				'image5':0,
+				'image6':0,
+				'image7':0,
+				'image8':0,
+				'image9':0
+			}
+function change_bg() {
+
+	const body=document.querySelector('body')
+	// console.log(i);
+	body.style.backgroundColor = val[i%2];
+	i=i+1;
+	// const body = document.getElementsByTagName('body');
+	// console.log(body)
+	// console.log()
+}
+
+function update(imageID) {
+	var im=document.getElementById(imageID.id)
+	// console.log(imageID);
+	im.src = docs[k%2];
+	k=k+1;
+	if(!visited[imageID.id]) visited[imageID.id]=1;
+
+	for (var key in visited) {
+    finish = finish + visited[key];
+    // console.log(finish,visited[key])
+}
+	console.log(finish)
+	if(finish==9) {
+		console.log("finished")
+		document.getElementById("finish").innerHTML = "GAME FINISHED";
+	}
+	finish = 0;
+>>>>>>> 9ee9647ced2acc110dc175bfe7362ef8a84a9d6b
 }
